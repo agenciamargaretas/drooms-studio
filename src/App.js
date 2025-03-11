5fb0731698c6c4721bf040b3df38d1816b02942d
Agência Margaretas <contato@agenciamargaretas.com>
2025-03-11 16:57:34 -0300

    Teste inicial do site Drooms com estilo brutalista

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DROOMS</h1>
+        <p>Estúdio de Música</p>
+        <div className="brutal-box">
+          <p>5 SALAS DE ENSAIO DISPONÍVEIS</p>
+          <button className="brutal-button">AGENDE AGORA</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
